5851d8e4e0691a85c34ca8c1e2c9c0d5f4771f34
Johnny Dreher <johnnydreher@gmail.com>
2025-11-21 17:39:39 -0300

    refactor: Simplify ShootCommand and tune system constants
    This commit refactors the shooting logic and updates various system constants.
    
    Key changes include:
    - Refactor `ShootCommand` state machine:
        - Remove `ConveyorTimer` and `Triggering` states in favor of immediate transitions based on sensor feedback and velocity.
        - Remove redundant telemetry and timer resets.
    - Update `ShooterConstants`:
        - Significantly increase `kP` and adjust `kF`.
        - Fine-tune hood regression coefficient `HOOD_N0`.
    - Integrate `UpdatePoseLimelightCommand` into `AutoShootCommand` to refresh pose before shooting.
    - Increase `COLOR_OFFSET` in `IndexerConstants`.
    - Disable the `Tuning` OpMode.
    
    Signed-off-by: Johnny Dreher <johnnydreher@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IndexerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IndexerConstants.java
@@ -22,6 +22,6 @@ public class IndexerConstants {
     /** The maximum number of game pieces the robot can hold. */
     public static int MAX_PIECE_CAPACITY = 3;
     /** The distance threshold in centimeters for the exit sensor to be considered triggered. */
-    public static double DISTANCE_OFFSET = 3.4;
+    public static double DISTANCE_OFFSET = 2.4;
     public static int COLOR_OFFSET = 120;
 }
