3fad5ffe98a5bfb400992ae52f21fb481add7d5d
TechMaker_FTC23069 <techmakerftc@gmail.com>
2025-11-21 15:11:11 -0300

    Uptade BluePoses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/paths/BlueRearPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/paths/BlueRearPoses.java
@@ -8,18 +8,19 @@ public class BlueRearPoses {
 
 
     public static final Pose[] POSES = {
-            new Pose(82.411, 8.399, Math.toRadians(90)),
-            new Pose(86.335, 17.147, Math.toRadians(68)),
 
-            new Pose(94.134, 36.169, Math.toRadians(180)),
-            new Pose(136.776, 36.169, Math.toRadians(180)),
-            new Pose(86.335, 17.147, Math.toRadians(68)),
+            new Pose(62.411, 8.399, Math.toRadians(90)),
+            new Pose(66.335, 17.147, Math.toRadians(68)),
+
+            new Pose(50.134, 34.169, Math.toRadians(180)),
+            new Pose(10.776, 34.169, Math.toRadians(180)),
+            new Pose(66.335, 17.147, Math.toRadians(68)),
 
 
-            new Pose(136, 20, Math.toRadians(95)),
-            new Pose(136, 6, Math.toRadians(90)),
-            new Pose(40, 36, Math.toRadians(90)),
+            new Pose(8, 23, Math.toRadians(110)),
+            new Pose(8, 6, Math.toRadians(90)),
 
+            new Pose(38, 34, Math.toRadians(90)),
 
     };
 
